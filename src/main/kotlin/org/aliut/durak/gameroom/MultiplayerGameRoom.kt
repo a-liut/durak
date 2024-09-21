5fb3c8667bd7761ecb3666a606dbd7cf5b30983b
a-liut <andrea.liut@gmail.com>
2024-09-21 18:05:04 +0200

    refactoring: created GameRoom abstraction improvement: improved process for waiting for players

---
+++ src/main/kotlin/org/aliut/durak/gameroom/MultiplayerGameRoom.kt
@@ -0,0 +1,71 @@
+package org.aliut.durak.gameroom
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.withContext
+import org.aliut.durak.game.player.Player
+import org.aliut.durak.game.player.RemotePlayer
+import java.net.ServerSocket
+
+private val logger = KotlinLogging.logger { }
+
+class MultiplayerGameRoom(
+    val playersCount: Int,
+    initialPlayers: Collection<Player>,
+    val socketPort: Int,
+) {
+    private val playersInRoom = mutableListOf<Player>()
+
+    init {
+        assert(initialPlayers.size <= playersCount)
+
+        playersInRoom.addAll(initialPlayers)
+    }
+
+    val players: List<Player>
+        get() = playersInRoom
+
+    val isFull: Boolean
+        get() = playersInRoom.size == playersCount
+
+    fun addPlayer(player: Player) {
+        if (isFull) {
+            throw IllegalStateException("Maximum number of players reached")
+        }
+
+        playersInRoom.add(player)
+    }
+
+    suspend fun waitPlayers() =
+        coroutineScope {
+            if (isFull) {
+                return@coroutineScope
+            }
+
+            withContext(Dispatchers.IO) {
+                ServerSocket(socketPort).use { server ->
+                    logger.debug { "Server running on port ${server.localPort}" }
+
+                    while (!isFull) {
+                        val client = server.accept()
+
+                        logger.debug { "Client connected: ${client.inetAddress.hostAddress}" }
+
+                        // Read the player's name
+                        val name = client.getInputStream().bufferedReader().readLine()
+
+                        val player = RemotePlayer(name, client)
+
+                        addPlayer(player)
+
+                        println("Player ${player.name} joined the game! players: ${players.size}/$playersCount")
+                    }
+                }
+            }
+
+            println("All players have joined the game!")
+
+            logger.debug { "Starting the game." }
+        }
+}
--- src/main/kotlin/org/aliut/durak/runner/MultiplayerGameRunner.kt
+++ src/main/kotlin/org/aliut/durak/runner/MultiplayerGameRunner.kt
@@ -2,13 +2,10 @@ package org.aliut.durak.runner
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.aliut.durak.game.player.LocalPlayer
 import org.aliut.durak.game.player.Player
-import org.aliut.durak.game.player.RemotePlayer
-import java.net.ServerSocket
+import org.aliut.durak.gameroom.MultiplayerGameRoom
 
 private val logger = KotlinLogging.logger { }
 
@@ -25,66 +22,13 @@ class MultiplayerGameRunner(private val config: MultiplayerGameConfig) : GameRun
         withContext(Dispatchers.Default) {
             println("Creating game room...")
 
-            // TODO: Verify concurrent access
-            val players: MutableList<Player> = mutableListOf(config.localPlayer)
+            val initialPlayers: MutableList<Player> = mutableListOf(config.localPlayer)
 
-            val remotePlayersToWait = config.playersCount - players.size
+            val gameRoom = MultiplayerGameRoom(config.playersCount, initialPlayers, config.port)
 
             println("Waiting for players to join...")
+            gameRoom.waitPlayers()
 
-            val playerChannel = Channel<RemotePlayer>()
-
-            val serverJob =
-                launch {
-                    startSocketServer(playerChannel, remotePlayersToWait)
-                }
-
-            val playerCollectorJob =
-                launch {
-                    for (player in playerChannel) {
-                        players.add(player)
-
-                        println("Player ${player.name} joined the game! players: ${players.size}/${config.playersCount}")
-                    }
-
-                    println("All players have joined the game!")
-                }
-
-            logger.debug { "Joining coroutines." }
-
-            runCatching { serverJob.join() }
-            runCatching { playerCollectorJob.join() }
-
-            logger.debug { "Coroutines joined." }
-
-            logger.debug { "Starting the game." }
-
-            players
+            gameRoom.players
         }
-
-    private suspend fun startSocketServer(
-        playerChannel: Channel<RemotePlayer>,
-        remotePlayersToAdd: Int,
-    ) = withContext(Dispatchers.IO) {
-        val server = ServerSocket(config.port)
-        logger.debug { "Server running on port ${server.localPort}" }
-
-        var joinedPlayers = 0
-        while (joinedPlayers < remotePlayersToAdd) {
-            val client = server.accept()
-
-            logger.debug { "Client connected: ${client.inetAddress.hostAddress}" }
-
-            // Read the player's name
-            val name = client.getInputStream().bufferedReader().readLine()
-
-            val player = RemotePlayer(name, client)
-
-            playerChannel.send(player)
-
-            joinedPlayers++
-        }
-
-        playerChannel.close()
-    }
 }
