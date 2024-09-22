45248cd2bdd7b489742e35c74b8ddfafcaff70e8
a-liut <andrea.liut@gmail.com>
2024-09-22 11:57:32 +0200

    feature: added banner

--- src/main/kotlin/org/aliut/durak/Main.kt
+++ src/main/kotlin/org/aliut/durak/Main.kt
@@ -3,4 +3,20 @@ package org.aliut.durak
 import com.github.ajalt.clikt.command.main
 import org.aliut.durak.menu.DurakMenu
 
-suspend fun main(args: Array<String>) = DurakMenu.create().main(args)
+suspend fun main(args: Array<String>) = showBanner().also { DurakMenu.create().main(args) }
+
+private fun showBanner() {
+    val banner =
+        """
+            ██████╗ ██╗   ██╗██████╗  █████╗ ██╗  ██╗
+            ██╔══██╗██║   ██║██╔══██╗██╔══██╗██║ ██╔╝
+            ██║  ██║██║   ██║██████╔╝███████║█████╔╝ 
+            ██║  ██║██║   ██║██╔══██╗██╔══██║██╔═██╗ 
+            ██████╔╝╚██████╔╝██║  ██║██║  ██║██║  ██╗
+            ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+                                                     
+        A classic russian game for terminal lovers.
+        """.trimIndent()
+
+    println(banner)
+}
