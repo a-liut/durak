8bee69b0b841537b10f91dbae273beb052d0fbb4
a-liut <andrea.liut@gmail.com>
2024-09-26 14:14:40 +0200

    refactoring: removed magic numbers from Rank

--- src/main/kotlin/org/aliut/durak/game/Rank.kt
+++ src/main/kotlin/org/aliut/durak/game/Rank.kt
@@ -1,13 +1,23 @@
 package org.aliut.durak.game
 
+private const val SIX_VALUE = 6
+private const val SEVEN_VALUE = 7
+private const val EIGHT_VALUE = 8
+private const val NINE_VALUE = 9
+private const val TEN_VALUE = 10
+private const val JACK_VALUE = 11
+private const val QUEEN_VALUE = 12
+private const val KING_VALUE = 13
+private const val ACE_VALUE = 14
+
 enum class Rank(val value: Int) {
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    JACK(11),
-    QUEEN(12),
-    KING(13),
-    ACE(14),
+    SIX(SIX_VALUE),
+    SEVEN(SEVEN_VALUE),
+    EIGHT(EIGHT_VALUE),
+    NINE(NINE_VALUE),
+    TEN(TEN_VALUE),
+    JACK(JACK_VALUE),
+    QUEEN(QUEEN_VALUE),
+    KING(KING_VALUE),
+    ACE(ACE_VALUE),
 }
